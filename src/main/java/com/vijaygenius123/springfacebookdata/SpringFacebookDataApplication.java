cd2d0c9bbac2383206b1a7864d36b90b26e08a6e
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-09-16 21:56:32 +0530

    Initial commit

---
+++ src/main/java/com/vijaygenius123/springfacebookdata/SpringFacebookDataApplication.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.springfacebookdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFacebookDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFacebookDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vijaygenius123/springfacebookdata/SpringFacebookDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.springfacebookdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFacebookDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
